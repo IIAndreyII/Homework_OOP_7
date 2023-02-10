5dca1fa8806ca4dafc3f266a68b3c460be1166fc
Андрей <volk.3894@yandex.ru>
2023-02-10 10:29:41 +0300

    add Homework7

---
+++ src/DataService.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface DataService {
+    void createUser(String firstName, String patronymic, String lastName);
+    List<User> getAll();
+    void deleteUser(Long id);
+}
